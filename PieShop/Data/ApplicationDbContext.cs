a9f1035c429cfc807329fc99c0166d5607e79ec8
bihter.yalcin <bihter.yalcin@BIHTERYALCIN>
2021-03-25 22:56:20 +0300

    Add project files.

---
+++ PieShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pie { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
---
+++ PieShop/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public int CategoryName { get; set; }
+        public int Description { get; set; }
+        public List<Pie> Pie { get; set; }
+
+    }
+}
